03cbd8fa8588f25a92aa98d811b4f9ff22f566b1
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-09-14 23:02:00 -0300

    Create game banner component

---
+++ src/components/GameBanner.tsx
@@ -0,0 +1,20 @@
+interface GameBannerProps {
+  banneUrl: string
+  title: string
+  adsCount: number
+}
+
+export function GameBanner({ adsCount, banneUrl, title }: GameBannerProps) {
+  return (
+    <a href="" className="relative rounded-lg overflow-hidden">
+      <img src={banneUrl} alt="" />
+
+      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+        <strong className="font-bold block">{title}</strong>
+        <span className="text-zinc-300 text-sm block">
+          {adsCount} anúncio(s)
+        </span>
+      </div>
+    </a>
+  )
+}
