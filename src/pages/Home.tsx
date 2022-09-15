7af08203be0682b1b5b617843348deed07b60d8c
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-09-15 00:35:35 -0300

    Implement fetch api for take games at api on the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,28 @@
-import LogoImg from '../assets/logo-nlw-esports.svg'
-import { MagnifyingGlassPlus } from 'phosphor-react'
+import { useEffect, useState } from 'react'
+
 import { GameBanner } from '../components/GameBanner'
 import { CreateAdBanner } from '../components/CreateAdBanner'
 
+import LogoImg from '../assets/logo-nlw-esports.svg'
+
+interface GameData {
+  id: string
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number
+  }
+}
+
 export function Home() {
+  const [games, setGames] = useState<GameData[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then((response) => response.json())
+      .then((data) => setGames(data))
+  }, [])
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={LogoImg} alt="" />
@@ -17,7 +36,16 @@ export function Home() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              banneUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
